d97898fcd4558da8743e732ee489753453fa0cb1
faniadifrihandoko <faniadifrihandoko1@gmail.com>
2020-10-18 01:25:58 +0700

    CV Html

---
+++ CV Html/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Tetaplah Hidup Walaupun Tidak Berguna");
+        System.out.println("--- Fani Adi Frihandoko ---");
+       
+    }
+
+}
---
+++ CV Html/tugas.java
@@ -0,0 +1,6 @@
+class a {
+    public static void main(string args[]){
+        system.out.println("Tetaplah Hidup Walaupun Tak Berguna");
+        
+    }
+}
